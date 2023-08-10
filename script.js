b7fcdb234ee15869d4797dd070df98db2b0f8bd0
Kev1nJ <kevinjerome85@yahoo.com>
2023-08-09 21:39:11 -0400

    Weather API fetch added, by coordinates. Weather data needs to be displayed in browser, and local memory capabilities need to be added

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 
-// var APIKey = "ab3b95adf307b5d880b55ba1eb4ff2bc";
+const apiKey = "ab3b95adf307b5d880b55ba1eb4ff2bc";
+const submitButton = document.getElementById('search-btn')
+function getWeather(city) {
+  const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+  fetch(cityURL)
+    .then(function (data) {
+      console.log(data);
+      return data.json()
+    })
+    .then(function (jsonData) {
+      console.log(jsonData);
+      const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${jsonData.coord.lat}&lon=${jsonData.coord.lon}&appid=${apiKey}&units=imperial`;
+
+      fetch(apiUrl)
+        .then(function (cityData) {
+          console.log(cityData);
+          return cityData.json()
+        })
+        .then(function (jsonCityData) {
+          console.log(jsonCityData)
+        })
+
+    })
+
+
+
+}
+submitButton.addEventListener('click', getDisplayWeather)
+function getDisplayWeather(event) {
+  console.log(event)
+  var cityName = event.target.previousElementSibling.value
+  console.log(cityName)
+  getWeather(cityName);
+}
+
+function displayWeatherData(data) { }
