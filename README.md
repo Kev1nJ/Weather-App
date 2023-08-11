2dff8b6716d3670b67ff3c19209b840a0f5dc5ac
Kev1nJ <kevinjerome85@yahoo.com>
2023-08-10 21:30:32 -0400

    incomplete final version

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Weather-App
+This Weather App has been created by Kevin Jerome and includes the following features
+- The ability to show the current temperature , wind speed, and humidity of a city entered into the search bar
+  - Using a fetch request for the OpenWeather API each city is found using longitutde and latitude for the location from there the city's name is found and the weather information is displayed.
+  -the console log is used to locate the path names and locations to access the API's data.
+  -the temperature is located and converted to imperial units for user experience.
+
+The app is currently missing some intended capabilities
+  - displaying the weather condition of the city that is being searched.
+  - saving user data in local storage to display previously searched cities underneath the search bar
+  - 5 Day forecast to see future weather conditions
--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
+
 const apiKey = "ab3b95adf307b5d880b55ba1eb4ff2bc";
 const submitButton = document.getElementById('search-btn')
+
+
+// fetch API key
 function getWeather(city) {
   const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
   fetch(cityURL)
@@ -11,6 +15,16 @@ function getWeather(city) {
     .then(function (jsonData) {
       console.log(jsonData);
       const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${jsonData.coord.lat}&lon=${jsonData.coord.lon}&appid=${apiKey}&units=imperial`;
+
+      currentWeather.city = jsonData.name;
+      
+
+      currentWeather.temp = jsonData.main.temp;
+      currentWeather.wind = jsonData.wind.speed;
+      currentWeather.humidity = jsonData.main.humidity;
+      console.log(currentWeather.humidity)
+      displayWeatherData(currentWeather);
+      
 
       fetch(apiUrl)
         .then(function (cityData) {
@@ -34,4 +48,34 @@ function getDisplayWeather(event) {
   getWeather(cityName);
 }
 
-function displayWeatherData(data) { }
+
+
+
+// Display weather in Content
+let currentWeather = {
+  city:"",
+  temp:"",
+  wind:"",
+  humidity:"",
+}
+
+
+
+
+function displayWeatherData(curWeather) {
+  const cityEl = document.getElementById('current-city');
+  const tempEl = document.getElementById('current-temp');
+  const windEl = document.getElementById('current-wind');
+  const humidEl = document.getElementById('current-humidity');
+  console.log(curWeather.humidity)
+
+  cityEl.innerHTML = curWeather.city;
+  tempEl.innerHTML = curWeather.temp;
+  windEl.innerHTML = curWeather.wind;
+  humidEl.innerHTML = curWeather.humidity;
+  
+ }
+
+ 
+ 
+ 
